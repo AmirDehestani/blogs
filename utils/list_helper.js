2b8523cb99655afc621d5106d9370670f3134456
Amir Dehestani <amira.dehestani@gmail.com>
2023-07-17 22:06:50 -0400

    Submit exercise 4.6 and 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -19,8 +21,46 @@ const favoriteBlog = (blogs) => {
   return favoriteSoFar;
 };
 
+const mostBlogs = (blogs) => {
+  const blogsCount = _.countBy(blogs, 'author');
+  const authorName = _.maxBy(
+    Object.keys(blogsCount),
+    (author) => blogsCount[author]
+  );
+  const numberOfBlogs = blogsCount[authorName];
+  return {
+    author: authorName,
+    blogs: numberOfBlogs,
+  };
+};
+
+const mostLikes = (blogs) => {
+  const likesCount = _.reduce(
+    blogs,
+    (result, blog) => {
+      if (blog.author in result === false) {
+        result[blog.author] = 0;
+      }
+      result[blog.author] += blog.likes;
+      return result;
+    },
+    {}
+  );
+  const authorName = _.maxBy(
+    Object.keys(likesCount),
+    (author) => likesCount[author]
+  );
+  const numberOfLikes = likesCount[authorName];
+  return {
+    author: authorName,
+    likes: numberOfLikes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
